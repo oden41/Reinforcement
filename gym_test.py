dd59b071281d80703daa289685d2d67167681fb1
oden <shumpmita@ic.dis.titech.ac.jp>
2018-02-01 18:12:33 +0900

    各種確認

--- gym_test.py
+++ gym_test.py
@@ -1,11 +1,17 @@
 import gym
+from gympackage.environment import Environment
 
-env = gym.make('CartPole-v0')
+env = gym.make('CartPole-v1')
 print(env)
 print(env.action_space)
 print(env.observation_space)
 print(env.reward_range)
 print(env.spec.max_episode_steps)#1エピソードの最大ステップ
 print(env.spec.timestep_limit)#タイムステップの最大
-print(env.spec.trials)
+print(env.spec.trials) #終了条件対象のエピソード数
 print(env.spec.reward_threshold)
+obs = env.reset()
+e = Environment()
+e.initialize()
+obs = e.next_step(1)
+print(obs)
